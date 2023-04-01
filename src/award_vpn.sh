7d5a80a4dee996ff4d18c2e60c988144d1d64eb1
ecclesias <77536370+zeviel@users.noreply.github.com>
2023-04-01 14:31:56 +0800

    Create award_vpn.sh

---
+++ src/award_vpn.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+api="https://api.award-vpn.com/v2"
+device_id=$(cat /dev/urandom | tr -dc "a-z0-9" | fold -w 24 | head -n 1)
+
+function get_token() {
+	response=$(curl --request POST \
+		--url "$api/token" \
+		--user-agent "okhttp/3.12.1" \
+		--header "content-type: application/x-www-form-urlencoded" \
+		--header "authorization: Basic YXBpLXVzZXI6Li5Bc2RmQDEyMzRqa2w7" \
+		--data "device=$device_id")
+	if [ -n $(jq -r ".token" <<< "$response") ]; then
+		token=$(jq -r ".token" <<< "$response")
+	fi
+	echo $response
+}
+
+function get_notifications() {
+	curl --request GET \
+		--url "$api/app/notifications" \
+		--user-agent "okhttp/3.12.1" \
+		--header "content-type: application/json" \
+		--header "authorization: Bearer $token"
+}
+
+function get_servers() {
+	curl --request GET \
+		--url "$api/servers" \
+		--user-agent "okhttp/3.12.1" \
+		--header "content-type: application/json" \
+		--header "authorization: Bearer $token"
+}
