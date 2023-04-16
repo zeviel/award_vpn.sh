a69892455430b24645eaaa16bea9bdec5a59691f
zeviel <77536370+zeviel@users.noreply.github.com>
2023-04-16 20:12:55 +0800

    Update award_vpn.sh

--- src/award_vpn.sh
+++ src/award_vpn.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 api="https://api.award-vpn.com/v2"
+user_agent="okhttp/3.12.1"
 device_id=$(cat /dev/urandom | tr -dc "a-z0-9" | fold -w 24 | head -n 1)
 
 function get_token() {
 	response=$(curl --request POST \
 		--url "$api/token" \
-		--user-agent "okhttp/3.12.1" \
+		--user-agent "$user_agent" \
 		--header "content-type: application/x-www-form-urlencoded" \
 		--header "authorization: Basic YXBpLXVzZXI6Li5Bc2RmQDEyMzRqa2w7" \
 		--data "device=$device_id")
@@ -19,7 +20,7 @@ function get_token() {
 function get_notifications() {
 	curl --request GET \
 		--url "$api/app/notifications" \
-		--user-agent "okhttp/3.12.1" \
+		--user-agent "$user_agent" \
 		--header "content-type: application/json" \
 		--header "authorization: Bearer $token"
 }
@@ -27,7 +28,7 @@ function get_notifications() {
 function get_servers() {
 	curl --request GET \
 		--url "$api/servers" \
-		--user-agent "okhttp/3.12.1" \
+		--user-agent "$user_agent" \
 		--header "content-type: application/json" \
 		--header "authorization: Bearer $token"
 }
